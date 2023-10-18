92d0871e55cef13b29fdd88132247c67bbd87d56
Sven Goetz <sgoetz@tesla.com>
2023-10-19 00:52:51 +0200

    Added approach with html2canvas usage directly.

--- src/app/shared/services/print.service.ts
+++ src/app/shared/services/print.service.ts
@@ -1,6 +1,7 @@
 import {ElementRef, Injectable} from '@angular/core';
 import {jsPDF} from "jspdf";
 import {PrintAttribute} from "./print.constants";
+import html2canvas from "html2canvas";
 
 @Injectable({
   providedIn: 'root'
@@ -8,28 +9,20 @@ import {PrintAttribute} from "./print.constants";
 export class PrintService {
 
   printPage(elRef: ElementRef) {
-    const doc = new jsPDF({
-      unit: 'px',
-      format: [595, 842]
-    });
-
-    doc.html(elRef.nativeElement, {
-      windowWidth: 595,
-      width: 595,
-      autoPaging: 'text',
-      html2canvas: {
-        ignoreElements: (element: HTMLElement) => {
-          return this.isBlacklistedByAttr(element);
-        }
-      },
-      callback: (doc: jsPDF) => {
-        doc.deletePage(doc.getNumberOfPages());
-        doc.save('pdf-export');
-      }
+    html2canvas(elRef.nativeElement, {
+      useCORS: true,
+      imageTimeout: 500,
+      ignoreElements: (element: Element) => this.isBlacklistedByAttr(element)
+    }).then(canvas => {
+      const doc = new jsPDF({
+        unit: 'px',
+        format: [canvas.width, canvas.height]
+      });
+      doc.addImage(canvas, 'px', 0, 0, canvas.width, canvas.height).save('pdf-export');
     });
   }
 
-  private isBlacklistedByAttr(element: HTMLElement) {
+  private isBlacklistedByAttr(element: Element) {
     return element.getAttribute(PrintAttribute) === 'false';
   }
 
